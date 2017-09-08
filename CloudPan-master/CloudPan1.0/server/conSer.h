90763b35afbf782f4d65202806f7599c51dcdbb7
王宇 <wangyucoder@foxmail.com>
2017-09-08 23:39:03 +0800

    Update conSer.h

--- CloudPan-master/CloudPan1.0/server/conSer.h
+++ CloudPan-master/CloudPan1.0/server/conSer.h
@@ -1,8 +1,8 @@
 /*************************************************************************
 	> File Name: conSer.h
-	> Author: qinyu
-	> Mail: qinyu.LT@gmail.com 
-	> Created Time: 2016年04月19日 星期二 22时51分03秒
+	> Author: wangyu
+	> Mail: wangyucoder@163.com 
+	> Created Time: 2016年07月19日 星期二 22时51分03秒
  ************************************************************************/
 
 #ifndef CONSER_H
