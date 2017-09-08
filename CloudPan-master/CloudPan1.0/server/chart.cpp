7403758fb6b042ae58a72931ff71923ca06486ff
王宇 <wangyucoder@foxmail.com>
2017-09-08 23:35:42 +0800

    Update chart.cpp

--- CloudPan-master/CloudPan1.0/server/chart.cpp
+++ CloudPan-master/CloudPan1.0/server/chart.cpp
@@ -1,8 +1,8 @@
 /*************************************************************************
 	> File Name: chart.cpp
-	> Author: qinyu
-	> Mail: qinyu.LT@gmail.com 
-	> Created Time: 2016年04月22日 星期五 22时40分18秒
+	> Author: wangyu
+	> Mail: wangyucoder@163.com 
+	> Created Time: 2016年07月22日 星期五 22时40分18秒
  ************************************************************************/
 
 #include "chart.h"
